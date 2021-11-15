63873473eefbff0a9f5df92015bb5f307faafdab
Samy Touabi <stoua026@uottawa.ca>
2021-11-15 17:56:25 -0500

    setPrice(0) et utilisation de assertTrue()

--- app/src/test/java/mgarzon/createbest/productcatalog/ProductTest.java
+++ app/src/test/java/mgarzon/createbest/productcatalog/ProductTest.java
@@ -20,11 +20,21 @@ public class ProductTest {
     public void checkProductPrice() {
         // I am making this test to fail
         Product aProduct = new Product("1", "DELL MONITOR", 180);
+        //assertEquals("Check the price of the product", "180.0", aProduct.getPrice()); WILL FAIL
+
+        // Il y a plusieurs problèmes avec la méthode checkPeoductPrice() initiale.
+        // Premièrement, assertEquals() vérigie si deux objets String sont les mêmes.
+        // Or, si on veut vérifier les prix, il font comparer les nombres. De toute façon,
+        // le prix de aProduct est 180 et non pas 0. Pour y remédier, on peut changer la
+        // valeur du prix de aProduct une fois l'objet instancié et ensuite utiliser la
+        // méthode importée assertTrue() qui vérifie si la condition donnée entre deux nombres
+        // est satisfaite.
+        //
 
 
-        //aProduct.setPrice(0);
-        //assertEquals("Check the price of the product", "180.0", aProduct.getPrice());
-        assertTrue(180 == aProduct.getPrice());
+        aProduct.setPrice(0);
+
+        assertTrue(0 == aProduct.getPrice());
     }
 
 
